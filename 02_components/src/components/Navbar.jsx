b3ce9af90a877810202a9253404a0d669ebbf010
andrew <andriidzivak3@gmail.com>
2026-08-26 11:41:47 +0300

    Initial commit

---
+++ 02_components/src/components/Navbar.jsx
@@ -0,0 +1,9 @@
+function Navbar() {
+    return (
+        <nav style={{ padding: '20px', borderBottom: '1px solid var(--border)', marginBottom: '20px' }}>
+            <h1 style={{ margin: 0, fontSize: '24px', color: 'var(--text-h)' }}>Літературні постаті</h1>
+        </nav>
+    );
+}
+
+export default Navbar;
