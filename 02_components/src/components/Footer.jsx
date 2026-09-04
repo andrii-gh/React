2878dc2ce7392da4f700ae18c2bbb8342a48e7f1
andrew <andriidzivak3@gmail.com>
2026-09-04 19:33:32 +0300

    Add projects 01, 02, 03

---
+++ 02_components/src/components/Footer.jsx
@@ -0,0 +1,9 @@
+function Footer() {
+    return (
+        <footer style={{ padding: '20px', borderTop: '1px solid var(--border)', marginTop: 'auto' }}>
+            <p>&copy; 2024 Літературні постаті. Всі права захищені.</p>
+        </footer>
+    );
+}
+
+export default Footer;
---
+++ 03_home_state/src/App.jsx
@@ -0,0 +1,14 @@
+import "./App.css";
+import Navbar from "./components/Navbar";
+import Counter from "./components/Counter";
+
+function App() {
+  return (
+    <div className="App">
+      <Navbar />
+      <Counter />
+    </div>
+  );
+}
+
+export default App;
